348e30af118f3e6539f93a6341ae7614017f0ed4
Can-A <calex6658@gmail.com>
2023-10-29 11:41:14 +0300

    stack header and props

--- src/screens/PlannerScreen.tsx
+++ src/screens/PlannerScreen.tsx
@@ -1,14 +1,15 @@
+import {NativeStackHeaderProps} from '@react-navigation/native-stack';
 import React, {useEffect} from 'react';
 import {View, Text, Button} from 'react-native';
 
-export const PlannerScreen = ({navigation}: any) => {
+export const PlannerScreen = ({navigation}: NativeStackHeaderProps) => {
   useEffect(() => {
     console.log('Rendering Planner Screen');
   }, []);
   return (
     <View>
       <Text>I am Planner screen</Text>
-      <Button title="Go to Home" onPress={() => navigation.push('Home')} />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
     </View>
   );
 };
