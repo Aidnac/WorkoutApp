5f299d02baeb91fffa63916315e796c2553473c3
Can-A <calex6658@gmail.com>
2023-10-29 17:15:38 +0300

    Styling

--- src/components/WorkOutItem.tsx
+++ src/components/WorkOutItem.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {StyleSheet, View, Text} from 'react-native';
 import {Workout} from '../types/data';
 
 export const WorkoutItem = ({item}: {item: Workout}) => {
   return (
-    <View>
-      <Text>{item.name}</Text>
-      <Text>{item.difficulty}</Text>
+    <View style={styles.container}>
+      <Text style={styles.name}>{item.name}</Text>
+      <Text style={styles.duration}>Duration: {item.duration}</Text>
+      <Text style={styles.difficulty}>Difficulty: {item.difficulty}</Text>
     </View>
   );
 };
- 
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 10,
+    borderColor: 'rgba(0,0,0,0.1',
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 10,
+    backgroundColor: '#fff',
+  },
+  name: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  duration: {
+    fontSize: 15,
+  },
+  difficulty: {
+    fontSize: 15,
+  },
+});
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -4,9 +4,10 @@ import data from '../../data.json';
 import {Workout} from '../types/data';
 import {WorkoutItem} from '../components/WorkOutItem';
 
-export const HomeScreen = () => { 
+export const HomeScreen = () => {
   return (
     <View style={styles.container}>
+      <Text style={styles.heading}>New Workouts</Text>
       <FlatList
         data={data as Workout[]}
         keyExtractor={item => item.slug}
@@ -18,6 +19,12 @@ export const HomeScreen = () => {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     padding: 20,
+  },
+  heading: {
+    fontSize: 20,
+    marginBottom: 20,
+    fontWeight: 'bold',
   },
 });
