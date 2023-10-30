93b7c4e11c547e45489ac613f19288b2252ce3d1
Can-A <calex6658@gmail.com>
2023-10-30 21:22:56 +0300

    Async sorage

--- src/hooks/useCachedResources.ts
+++ src/hooks/useCachedResources.ts
@@ -1,11 +1,22 @@
 import {useState, useEffect} from 'react';
+import {getData, storeData} from '../storage';
+import data from '../../data.json';
 
 export const useCacheResources = () => {
   const [isLoadingComplete, setIsLoadingComplete] = useState(false);
 
   useEffect(() => {
-    const loadResourcesAndDataAsync = () => {
-      //This was going to be used for loading expo fonts
+    const loadResourcesAndDataAsync = async () => {
+      //This was going to be used for loading staff we need beforeapp sstarts
+      try {
+        await storeData('workout-data', data);
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        const workouts = await getData('workout-data');
+        console.log(workouts)
+        setIsLoadingComplete(true);
+      }
     };
 
     loadResourcesAndDataAsync();
